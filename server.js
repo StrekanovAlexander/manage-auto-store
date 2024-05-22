d2c93844a9e2df1f39c677ec04750a96379d011d
Alexander Strekanov <strekanov.alexander@gmail.com>
2024-05-22 22:26:15 +0200

    added: static middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { brand, home } from './app/routes/index.js';
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/', home);
 app.use('/brand', brand);
